64543f3e31118c4b422ff7a18b864ef55728fa18
Bryan Anderson <bryannobleanderson@live.com>
2016-08-31 18:56:36 -0600

    added model specs

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe School, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+      it { should validate_presence_of(:name, :type) }
+    end
+
+  describe "#school_type" do
+    it "returns public of true" do
+        item = Item.create(name: 'school', type: true)
+        expect(school.school_type).to eq("Public")
+    end
+  end
+
+  it "returns rawr if scary" do
+      item = Item.create(name: 'school', type: false)
+      expect(school.school_type).to eq("Private")
+  end
 end
